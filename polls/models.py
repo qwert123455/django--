fcb914b29622aa1e214d616705a41a3da778f177
maozhaojun <1807119680@qq.com>
2018-12-17 12:05:29 +0800

    part2结束

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta,datetime
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField('问题内容',max_length=200)
-    pub_data = models.DateTimeField('发布时间')
+    pub_date = models.DateTimeField('发布时间')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        if timezone.now() - timedelta(days=1) <= self.pub_date:
+            return True
+        else:
+            return False
+
+        
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField('选项内容',max_length=200)
